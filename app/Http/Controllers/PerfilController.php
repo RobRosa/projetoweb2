47cb1d29deb733404d9560d6b7839d16ad923830
Robson Rosa <robrda@hotmail.com>
2018-10-17 23:48:03 -0300

    Finalizado: sistema de autenticação de usuário

---
+++ app/Http/Controllers/PerfilController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace projetoWeb2\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PerfilController extends Controller
+{
+    public function index(){
+    	return view('perfil/perfil');
+    }
+}
