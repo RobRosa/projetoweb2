056399fc8e0089d78695c2d2d8826ae545c974be
Robson Rosa <robrda@hotmail.com>
2018-10-28 16:12:06 -0300

    Melhorias na classe de usuário. Novos campos de registro.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->char('cpf', 11);
+            $table->date('data_nascimeto');
             $table->rememberToken();
             $table->timestamps();
         });
