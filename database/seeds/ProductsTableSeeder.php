9feaed381285adcbb6a47cd9c3e9d0c357f584b6
Robson Rosa <robrda@hotmail.com>
2018-11-02 20:07:58 -0300

    Criação do seeder de Produtos e correção na tabela de listagem

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsTableSeeder extends Seeder
+{
+
+    private static $qtdProducts = 15;
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    private function getProdString($qtdPalavras){
+    	$desc = '';
+    	for ($i=0; $i < $qtdPalavras; $i++) { 
+    		$desc .= trim(str_random(mt_rand(1, 20))) . ' ';
+    	}
+
+    	return $desc;
+    }
+
+    public function run()
+    {
+    	$arrProducts = [];
+
+    	for($i = 0; $i < self::$qtdProducts; $i++){
+    		$priceCents = mt_rand(0, 99);
+    		$product = [
+        		'name' 			=> $this->getProdString(random_int(1, 8)),
+		        'description' 	=> $this->getProdString(random_int(1, 15)),
+		        'brand' 		=> str_random(random_int(1, 20)),
+		        'color' 		=> str_random(random_int(1, 20)),
+		        'price' 		=> mt_rand(1, 10000) + ($priceCents > 0 ? $priceCents / 100 : 0),
+				'amount' 		=> random_int(0, 1000)
+        	];
+
+        	array_push($arrProducts, $product);
+    	}
+
+        DB::table('products')->insert($arrProducts);
+    }
+}
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -8,43 +8,46 @@
 			<div class="col-md-2">
 				<a class="btn btn-primary" href="{{ route('product.create') }}" title="Create">Create</a>
 			</div>
-		</div>
 		@if ($message = Session::get('success'))
 			<div class="alert alert-success">
 				<p>{{ $message }}</p>
 			</div>
 		@endif
-		<table class="table">
-			<tr>
-				<th>Nro</th>
-				<th>Name</th>
-				<th>Description</th>
-				<th>Color</th>
-				<th>Price</th>
-				<th>Amount</th>
-				<th>Btn</th>
-			</tr>
-			@foreach ($products as $row)
+		<div class="col-md-12">
+			<table class="table">
 				<tr>
-					<td>{{ ++$i }}.</td>
-					<td>{{ $row->name }}</td>
-					<td>{{ $row->description }}</td>
-					<td>{{ $row->brand }}</td>
-					<td>{{ $row->color }}</td>
-					<td>{{ $row->price }}</td>
-					<td>{{ $row->amount }}</td>
-					<td>
-						<form action="{{ route('product.destroy', $row->id) }}" method="post">
-							<a class="btn btn-success" href="{{ route('product.show', $row->id) }}" title="show">Show</a>
-							<a class="btn btn-warning" href="{{ route('product.edit', $row->id) }}" title="edit">Edit</a>
-							@csrf
-							@method('DELETE')
-							<button class="btn btn-danger" type="submit">Delete</button>
-						</form>
-					</td>
+					<th>Nro</th>
+					<th>Name</th>
+					<th>Description</th>
+					<th>Brand</th>
+					<th>Color</th>
+					<th>Price</th>
+					<th>Amount</th>
+					<th>Btn</th>
 				</tr>
-			@endforeach
-		</table>
+				@foreach ($products as $row)
+					<tr>
+						<td>{{ ++$i }}.</td>
+						<td>{{ $row->name }}</td>
+						<td>{{ $row->description }}</td>
+						<td>{{ $row->brand }}</td>
+						<td>{{ $row->color }}</td>
+						<td>{{ $row->price }}</td>
+						<td>{{ $row->amount }}</td>
+						<td>
+							<form action="{{ route('product.destroy', $row->id) }}" method="post">
+								<a class="btn btn-success" href="{{ route('product.show', $row->id) }}" title="show">Show</a>
+								<a class="btn btn-warning" href="{{ route('product.edit', $row->id) }}" title="edit">Edit</a>
+								@csrf
+								@method('DELETE')
+								<button class="btn btn-danger" type="submit">Delete</button>
+							</form>
+						</td>
+					</tr>
+				@endforeach
+			</table>
+		</div>
 		{!! $products->links() !!}
+		</div>
 	</div>
 @endsection
