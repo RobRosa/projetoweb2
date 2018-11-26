09849d2f1f5be4af2d632f42c4b2e792d7b77895
rrscotti <rodrigo.scotti1@gmail.com>
2018-11-26 00:35:49 -0200

    Atualizando o branch

---
+++ app/Address.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace projetoweb2;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Address extends Model
+{
+
+	protected $table 	= 'enderecos';
+	protected $fillable = [
+		'cep',
+		'endereco',
+		'numero',
+		'complemento',
+		'cidade',
+		'estado'
+	];
+	
+}
